419fa1cd69114fd88531734b727c0ec7b2fec149
Hugo Morgan <hpjm37@gmail.com>
2021-02-09 10:27:12 +0000

    test CryptoApis fetch logic

--- lib/crypto_apis.ex
+++ lib/crypto_apis.ex
@@ -3,16 +3,68 @@ defmodule CryptoApis do
   Documentation for `CryptoApis`.
   """
 
-  @doc """
-  Hello world.
+  defp handle_response(
+         {_status, %HTTPoison.Response{status_code: status_code, headers: headers} = resp}
+       )
+       when status_code >= 200 and status_code < 300 do
+    if json?(headers), do: parse_json(resp), else: {:ok, resp}
+  end
 
-  ## Examples
+  defp handle_response({_status, %HTTPoison.Response{} = resp}) do
+    {:error, resp}
+  end
 
-      iex> CryptoApis.hello()
-      :world
+  defp handle_response({:error, _error} = result), do: result
 
-  """
-  def hello do
-    :world
+  defp handle_response!(%HTTPoison.Response{status_code: status_code, headers: headers} = resp)
+       when status_code >= 200 and status_code < 300 do
+    if json?(headers), do: parse_json!(resp), else: resp
+  end
+
+  defp handle_response!(%HTTPoison.Response{} = resp) do
+    resp
+  end
+
+  defp parse_json(%HTTPoison.Response{body: body} = resp) do
+    case Jason.decode(body) do
+      {:ok, body} -> {:ok, %HTTPoison.Response{resp | body: body}}
+      {:error, _error} = result -> result
+    end
+  end
+
+  defp parse_json!(%HTTPoison.Response{body: body} = resp) do
+    %HTTPoison.Response{resp | body: Jason.decode!(body)}
+  end
+
+  defp json?(headers) when is_list(headers) do
+    Enum.find_value(headers, false, &json_header?/1)
+  end
+
+  defp json_header?({"Content-Type", value}) do
+    String.contains?(value, "application/json")
+  end
+
+  defp json_header?(_value), do: false
+
+  def fetch(url, opts \\ []) when is_binary(url) do
+    {options, headers} = get_opts(opts)
+
+    url
+    |> HTTPoison.get(headers, options)
+    |> handle_response()
+  end
+
+  def fetch!(url, opts \\ []) when is_binary(url) do
+    {options, headers} = get_opts(opts)
+
+    url
+    |> HTTPoison.get!(headers, options)
+    |> handle_response!()
+  end
+
+  defp get_opts(opts) do
+    options = Keyword.get(opts, :options, [])
+    headers = Keyword.get(opts, :headers, [])
+    {options, headers}
   end
 end
--- test/crypto_apis_test.exs
+++ test/crypto_apis_test.exs
@@ -1,8 +1,116 @@
 defmodule CryptoApisTest do
   use ExUnit.Case
   doctest CryptoApis
+  import Mock
 
-  test "greets the world" do
-    assert CryptoApis.hello() == :world
+  @data %{key: :value}
+  @encoded Jason.encode!(@data)
+  @decoded Jason.decode!(@encoded)
+  @url "http://localhost:4000"
+  @json_headers [{"Content-Type", "application/json"}]
+
+  def successful_response(headers \\ [], body \\ @encoded) do
+    %HTTPoison.Response{status_code: 200, body: body, headers: headers}
+  end
+
+  def error_response(status_code \\ 404) do
+    %HTTPoison.Response{status_code: status_code}
+  end
+
+  describe "fetch/2" do
+    test "fetch returns successful non-json responses" do
+      with_mock HTTPoison,
+        get: fn _url, _headers, _options ->
+          {:ok, successful_response()}
+        end do
+        assert CryptoApis.fetch(@url) ==
+                 {:ok, %HTTPoison.Response{body: @encoded, status_code: 200}}
+      end
+    end
+
+    test "fetch returns successful json responses" do
+      with_mock HTTPoison,
+        get: fn _url, _headers, _options ->
+          {:ok, successful_response(@json_headers)}
+        end do
+        assert CryptoApis.fetch(@url) ==
+                 {:ok,
+                  %HTTPoison.Response{
+                    body: @decoded,
+                    status_code: 200,
+                    headers: @json_headers
+                  }}
+      end
+    end
+
+    test "fetch returns error responses for any error status code outside 200-300" do
+      with_mock HTTPoison,
+        get: fn _url, _headers, _options ->
+          {:error, error_response()}
+        end do
+        assert CryptoApis.fetch(@url) ==
+                 {:error,
+                  %HTTPoison.Response{
+                    status_code: 404
+                  }}
+      end
+    end
+
+    test "fetch returns error responses for json error" do
+      with_mock HTTPoison,
+        get: fn _url, _headers, _options ->
+          {:ok, successful_response(@json_headers, "bad json")}
+        end do
+        assert {:error, %Jason.DecodeError{}} = CryptoApis.fetch(@url)
+      end
+    end
+  end
+
+  describe "fetch!/2" do
+    test "fetch! returns successful non-json responses" do
+      with_mock HTTPoison,
+        get!: fn _url, _headers, _options ->
+          successful_response()
+        end do
+        assert CryptoApis.fetch!(@url) ==
+                 %HTTPoison.Response{body: @encoded, status_code: 200}
+      end
+    end
+
+    test "fetch! returns successful json responses" do
+      with_mock HTTPoison,
+        get!: fn _url, _headers, _options ->
+          successful_response(@json_headers)
+        end do
+        assert CryptoApis.fetch!(@url) ==
+                 %HTTPoison.Response{
+                   body: @decoded,
+                   status_code: 200,
+                   headers: @json_headers
+                 }
+      end
+    end
+
+    test "fetch! does not raise for error codes" do
+      with_mock HTTPoison,
+        get!: fn _url, _headers, _options ->
+          error_response(500)
+        end do
+        assert %HTTPoison.Response{
+          status_code: 500
+        }
+      end
+    end
+
+    test "fetch! returns error responses for json error" do
+      with_mock HTTPoison,
+        get!: fn _url, _headers, _options ->
+          successful_response(@json_headers, "bad json")
+        end do
+        assert_raise Jason.DecodeError, fn ->
+          CryptoApis.fetch!(@url)
+        end
+      end
+    end
   end
 end
