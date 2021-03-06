767c2342c18f19b56df3a6a6a3e73bd3d2e3443d
Hugo Morgan <hpjm37@gmail.com>
2021-03-06 11:45:21 +0000

    test util

--- test/utils/utils_test.exs
+++ test/utils/utils_test.exs
@@ -1,10 +1,42 @@
 defmodule CryptoApis.UtilsTest do
   use ExUnit.Case
 
+  alias CryptoApis.Utils
+
   describe "split_pair/1" do
     test "splits pair into crypto and fiat" do
-      assert CryptoApis.Utils.split_pair("BTCJPY") == {"BTC", "JPY"}
-      assert CryptoApis.Utils.split_pair(:USDTGBP) == {"USDT", "GBP"}
+      assert Utils.split_pair("BTCJPY") == {"BTC", "JPY"}
+      assert Utils.split_pair(:USDTGBP) == {"USDT", "GBP"}
+    end
+  end
+
+  describe "pair_to_tuple/1" do
+    test "converts from string" do
+      assert Utils.pair_to_tuple("BTCJPY") == {"BTC", "JPY"}
+    end
+
+    test "converts from atom" do
+      assert Utils.pair_to_tuple(:BTCJPY) == {"BTC", "JPY"}
+    end
+  end
+
+  describe "pair_to_string/2" do
+    test "converts from string" do
+      assert Utils.pair_to_string("BTCVND") == "BTCVND"
+    end
+
+    test "converts from atom" do
+      assert Utils.pair_to_string(:BTCVND) == "BTCVND"
+    end
+
+    test "converts from tuple" do
+      assert Utils.pair_to_string({"BTC", "VND"}) == "BTCVND"
+    end
+
+    test "works with separator" do
+      assert Utils.pair_to_string({"BTC", "VND"}, "_") == "BTC_VND"
+      assert Utils.pair_to_string(:BTCGBP, ".") == "BTC.GBP"
+      assert Utils.pair_to_string("BTCUSD", "-") == "BTC-USD"
     end
   end
 end
