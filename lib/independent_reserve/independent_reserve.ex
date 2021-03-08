05f53caf9d80b7be297651b0671e755ca0302541
Hugo Morgan <hpjm37@gmail.com>
2021-03-08 16:37:03 +0000

    refactor independent reserve

--- lib/independent_reserve/independent_reserve.ex
+++ lib/independent_reserve/independent_reserve.ex
@@ -1,36 +1,88 @@
 defmodule CryptoApis.IndependentReserve do
   @moduledoc """
-  An API wrapper for the Independent Reserve exchange.
-
   https://www.independentreserve.com/products/api#public-methods
-
-  Currently only supports public endpoints.
   """
 
   @root_url "https://api.independentreserve.com/Public"
 
-  alias CryptoApis
+  alias CryptoApis.Pair
 
-  defp get_pair({_, _} = pair), do: pair
-  defp get_pair(pair), do: CryptoApis.Utils.split_pair(pair)
+  defp process_pair(pair), do: pair |> Pair.new() |> Pair.to_tuple()
   defp parse_crypto("BTC"), do: "xbt"
   defp parse_crypto(crypto), do: String.downcase(crypto)
 
-  defp get(type, pair, opts) do
-    {crypto, fiat} = get_pair(pair)
+  defp get_params(pair, params \\ []) do
+    {crypto, fiat} = process_pair(pair)
 
-    params = [
-      primaryCurrencyCode: parse_crypto(crypto),
-      secondaryCurrencyCode: String.downcase(fiat)
+    [
+      params:
+        [
+          primaryCurrencyCode: parse_crypto(crypto),
+          secondaryCurrencyCode: String.downcase(fiat)
+        ] ++ params
     ]
+  end
 
+  defp get(type, pair, opts) do
     type
     |> url()
-    |> CryptoApis.get(opts ++ [params: params])
+    |> CryptoApis.get(opts ++ get_params(pair))
+  end
+
+  defp url(:trade_history) do
+    @root_url <> "/GetTradeHistorySummary"
+  end
+
+  defp url(:recent_trades) do
+    @root_url <> "/GetRecentTrades"
   end
 
   defp url(:orders) do
     @root_url <> "/GetOrderBook"
+  end
+
+  defp url(:all_orders) do
+    @root_url <> "/GetAllOrders"
+  end
+
+  defp url(:market_summary) do
+    @root_url <> "/GetMarketSummary"
+  end
+
+  defp url(:cryptos) do
+    @root_url <> "/GetValidPrimaryCurrencyCodes"
+  end
+
+  defp url(:fiats) do
+    @root_url <> "/GetValidSecondaryCurrencyCodes"
+  end
+
+  defp url(:limit_order_types) do
+    @root_url <> "/GetValidLimitOrderTypes"
+  end
+
+  defp url(:market_order_types) do
+    @root_url <> "/GetValidMarketOrderTypes"
+  end
+
+  defp url(:order_types) do
+    @root_url <> "/GetValidOrderTypes"
+  end
+
+  defp url(:transaction_types) do
+    @root_url <> "/GetValidTransactionTypes"
+  end
+
+  defp url(:fx_rates) do
+    @root_url <> "/GetFxRates"
+  end
+
+  defp url(:minimum_volumes) do
+    @root_url <> "/GetOrderMinimumVolumes"
+  end
+
+  defp url(:withdrawal_fees) do
+    @root_url <> "/GetCryptoWithdrawalFees"
   end
 
   @doc """
@@ -38,5 +90,104 @@ defmodule CryptoApis.IndependentReserve do
   """
   def order_book(pair, opts \\ []) do
     get(:orders, pair, opts)
+  end
+
+  @doc """
+  https://www.independentreserve.com/products/api#GetAllOrders
+  """
+  def all_orders(pair, opts \\ []) do
+    get(:all_orders, pair, opts)
+  end
+
+  @doc """
+  https://www.independentreserve.com/products/api#GetMarketSummary
+  """
+  def market_summary(pair, opts \\ []) do
+    get(:market_summary, pair, opts)
+  end
+
+  @doc """
+  https://www.independentreserve.com/products/api#GetTradeHistorySummary
+  """
+  def trade_history(pair, hours_in_past, opts \\ []) do
+    get(
+      :trade_history,
+      pair,
+      opts ++ get_params(pair, numberOfHoursInThePastToRetrieve: hours_in_past)
+    )
+  end
+
+  @doc """
+  https://www.independentreserve.com/products/api#GetRecentTrades
+  """
+  def recent_trades(pair, num_trades, opts \\ []) do
+    get(
+      :recent_trades,
+      pair,
+      opts ++ get_params(pair, numberOfRecentTradesToRetrieve: num_trades)
+    )
+  end
+
+  @doc """
+  https://www.independentreserve.com/products/api#GetValidPrimaryCurrencyCodes
+  """
+  def cryptos(opts \\ []) do
+    :cryptos |> url() |> CryptoApis.get(opts)
+  end
+
+  @doc """
+  https://www.independentreserve.com/products/api#GetValidSecondaryCurrencyCodes
+  """
+  def fiats(opts \\ []) do
+    :fiats |> url() |> CryptoApis.get(opts)
+  end
+
+  @doc """
+  https://www.independentreserve.com/products/api#GetValidLimitOrderTypes
+  """
+  def limit_order_types(opts \\ []) do
+    :limit_order_types |> url() |> CryptoApis.get(opts)
+  end
+
+  @doc """
+  https://www.independentreserve.com/products/api#GetValidMarketOrderTypes
+  """
+  def market_order_types(opts \\ []) do
+    :market_order_types |> url() |> CryptoApis.get(opts)
+  end
+
+  @doc """
+  https://www.independentreserve.com/products/api#GetValidOrderTypes
+  """
+  def order_types(opts \\ []) do
+    :order_types |> url() |> CryptoApis.get(opts)
+  end
+
+  @doc """
+  https://www.independentreserve.com/products/api#GetFxRates
+  """
+  def fx_rates(opts \\ []) do
+    :fx_rates |> url() |> CryptoApis.get(opts)
+  end
+
+  @doc """
+  https://www.independentreserve.com/products/api#GetValidTransactionTypes
+  """
+  def transaction_types(opts \\ []) do
+    :transaction_types |> url() |> CryptoApis.get(opts)
+  end
+
+  @doc """
+  https://www.independentreserve.com/products/api#GetOrderMinimumVolumes
+  """
+  def minimum_volumes(opts \\ []) do
+    :minimum_volumes |> url() |> CryptoApis.get(opts)
+  end
+
+  @doc """
+  https://www.independentreserve.com/products/api#GetCryptoWithdrawalFees
+  """
+  def withdrawal_fees(opts \\ []) do
+    :withdrawal_fees |> url() |> CryptoApis.get(opts)
   end
 end
--- test/independent_reserve/independent_reserve_test.exs
+++ test/independent_reserve/independent_reserve_test.exs
@@ -22,4 +22,215 @@ defmodule CryptoApis.IndependentReserveTest do
       end
     end
   end
+
+  describe "market_summary" do
+    test "market_summary/1 responds ok" do
+      with_mock HTTPoison,
+        get: fn url, _headers, options ->
+          {:ok, successful_response(url: url, options: options)}
+        end do
+        assert {:ok, response} = IndependentReserve.market_summary(:BTCAUS)
+        assert response.status_code == 200
+
+        assert response.request_url ==
+                 "https://api.independentreserve.com/Public/GetMarketSummary"
+
+        assert response.request.options == [
+                 params: [primaryCurrencyCode: "xbt", secondaryCurrencyCode: "aus"]
+               ]
+      end
+    end
+  end
+
+  describe "trade_history" do
+    test "trade_history/2 responds ok" do
+      with_mock HTTPoison,
+        get: fn url, _headers, options ->
+          {:ok, successful_response(url: url, options: options)}
+        end do
+        assert {:ok, response} = IndependentReserve.trade_history(:BTCAUS, 1)
+        assert response.status_code == 200
+
+        assert response.request_url ==
+                 "https://api.independentreserve.com/Public/GetTradeHistorySummary"
+
+        assert response.request.options == [
+                 params: [
+                   primaryCurrencyCode: "xbt",
+                   secondaryCurrencyCode: "aus",
+                   numberOfHoursInThePastToRetrieve: 1
+                 ]
+               ]
+      end
+    end
+  end
+
+  describe "recent_trades" do
+    test "recent_trades/2 responds ok" do
+      with_mock HTTPoison,
+        get: fn url, _headers, options ->
+          {:ok, successful_response(url: url, options: options)}
+        end do
+        assert {:ok, response} = IndependentReserve.recent_trades(:BTCAUS, 1)
+        assert response.status_code == 200
+
+        assert response.request_url ==
+                 "https://api.independentreserve.com/Public/GetRecentTrades"
+
+        assert response.request.options == [
+                 params: [
+                   primaryCurrencyCode: "xbt",
+                   secondaryCurrencyCode: "aus",
+                   numberOfRecentTradesToRetrieve: 1
+                 ]
+               ]
+      end
+    end
+  end
+
+  describe "cryptos" do
+    test "cryptos/0 responds ok" do
+      with_mock HTTPoison,
+        get: fn url, _headers, options ->
+          {:ok, successful_response(url: url, options: options)}
+        end do
+        assert {:ok, response} = IndependentReserve.cryptos()
+        assert response.status_code == 200
+
+        assert response.request_url ==
+                 "https://api.independentreserve.com/Public/GetValidPrimaryCurrencyCodes"
+      end
+    end
+  end
+
+  describe "fiats" do
+    test "fiats/0 responds ok" do
+      with_mock HTTPoison,
+        get: fn url, _headers, options ->
+          {:ok, successful_response(url: url, options: options)}
+        end do
+        assert {:ok, response} = IndependentReserve.fiats()
+        assert response.status_code == 200
+
+        assert response.request_url ==
+                 "https://api.independentreserve.com/Public/GetValidSecondaryCurrencyCodes"
+      end
+    end
+  end
+
+  describe "limit_order_types" do
+    test "limit_order_types/0 responds ok" do
+      with_mock HTTPoison,
+        get: fn url, _headers, options ->
+          {:ok, successful_response(url: url, options: options)}
+        end do
+        assert {:ok, response} = IndependentReserve.limit_order_types()
+        assert response.status_code == 200
+
+        assert response.request_url ==
+                 "https://api.independentreserve.com/Public/GetValidLimitOrderTypes"
+      end
+    end
+
+    test "market_order_types/0 responds ok" do
+      with_mock HTTPoison,
+        get: fn url, _headers, options ->
+          {:ok, successful_response(url: url, options: options)}
+        end do
+        assert {:ok, response} = IndependentReserve.market_order_types()
+        assert response.status_code == 200
+
+        assert response.request_url ==
+                 "https://api.independentreserve.com/Public/GetValidMarketOrderTypes"
+      end
+    end
+
+    test "transaction_types/0 responds ok" do
+      with_mock HTTPoison,
+        get: fn url, _headers, options ->
+          {:ok, successful_response(url: url, options: options)}
+        end do
+        assert {:ok, response} = IndependentReserve.transaction_types()
+        assert response.status_code == 200
+
+        assert response.request_url ==
+                 "https://api.independentreserve.com/Public/GetValidTransactionTypes"
+      end
+    end
+  end
+
+  describe "market_order_types" do
+    test "market_order_types/0 responds ok" do
+      with_mock HTTPoison,
+        get: fn url, _headers, options ->
+          {:ok, successful_response(url: url, options: options)}
+        end do
+        assert {:ok, response} = IndependentReserve.market_order_types()
+        assert response.status_code == 200
+
+        assert response.request_url ==
+                 "https://api.independentreserve.com/Public/GetValidMarketOrderTypes"
+      end
+    end
+  end
+
+  describe "transaction_types" do
+    test "transaction_types/0 responds ok" do
+      with_mock HTTPoison,
+        get: fn url, _headers, options ->
+          {:ok, successful_response(url: url, options: options)}
+        end do
+        assert {:ok, response} = IndependentReserve.transaction_types()
+        assert response.status_code == 200
+
+        assert response.request_url ==
+                 "https://api.independentreserve.com/Public/GetValidTransactionTypes"
+      end
+    end
+  end
+
+  describe "fx_rates" do
+    test "fx_rates/0 responds ok" do
+      with_mock HTTPoison,
+        get: fn url, _headers, options ->
+          {:ok, successful_response(url: url, options: options)}
+        end do
+        assert {:ok, response} = IndependentReserve.fx_rates()
+        assert response.status_code == 200
+
+        assert response.request_url ==
+                 "https://api.independentreserve.com/Public/GetFxRates"
+      end
+    end
+  end
+
+  describe "minimum_volumes" do
+    test "minimum_volumes/0 responds ok" do
+      with_mock HTTPoison,
+        get: fn url, _headers, options ->
+          {:ok, successful_response(url: url, options: options)}
+        end do
+        assert {:ok, response} = IndependentReserve.minimum_volumes()
+        assert response.status_code == 200
+
+        assert response.request_url ==
+                 "https://api.independentreserve.com/Public/GetOrderMinimumVolumes"
+      end
+    end
+  end
+
+  describe "withdrawal_fees" do
+    test "withdrawal_fees/0 responds ok" do
+      with_mock HTTPoison,
+        get: fn url, _headers, options ->
+          {:ok, successful_response(url: url, options: options)}
+        end do
+        assert {:ok, response} = IndependentReserve.withdrawal_fees()
+        assert response.status_code == 200
+
+        assert response.request_url ==
+                 "https://api.independentreserve.com/Public/GetCryptoWithdrawalFees"
+      end
+    end
+  end
 end
