90e3d5d3186d1671039514526343e6a3783a4c7c
Hugo Morgan <hpjm37@gmail.com>
2021-03-08 12:18:26 +0000

    refactor bitstamp

--- lib/bitstamp/bitstamp.ex
+++ lib/bitstamp/bitstamp.ex
@@ -1,21 +1,18 @@
 defmodule CryptoApis.Bitstamp do
   @moduledoc """
-  An API wrapper for the bitFlyer exchange.
-
-  Docs: https://www.bitstamp.net/api/
-
-  Currently only supports public endpoints.
+  https://www.bitstamp.net/api/
   """
 
   @root_url "https://www.bitstamp.net/api/v2"
 
-  alias CryptoApis
+  alias CryptoApis.Pair
 
-  defp get_pair({crypto, fiat}), do: "#{crypto}#{fiat}"
-  defp get_pair(pair), do: "#{pair}"
+  defp process_pair(pair) do
+    pair |> Pair.new() |> Pair.join(downcase?: true)
+  end
 
   defp get(type, pair, opts) do
-    pair = pair |> get_pair() |> String.downcase()
+    pair = process_pair(pair)
 
     type
     |> url(pair)
@@ -30,8 +27,20 @@ defmodule CryptoApis.Bitstamp do
     "#{@root_url}/ticker/#{pair}/"
   end
 
+  defp url(:hourly_ticker, pair) do
+    "#{@root_url}/ticker_hour/#{pair}/"
+  end
+
   defp url(:trades, pair) do
     "#{@root_url}/transactions/#{pair}/"
+  end
+
+  defp url(:ohlc, pair) do
+    "#{@root_url}/ohlc/#{pair}/"
+  end
+
+  defp url(:pairs) do
+    "#{@root_url}/trading-pairs-info/"
   end
 
   def order_book(pair, opts \\ []) do
@@ -42,7 +51,21 @@ defmodule CryptoApis.Bitstamp do
     get(:ticker, pair, opts)
   end
 
+  def hourly_ticker(pair, opts \\ []) do
+    get(:hourly_ticker, pair, opts)
+  end
+
   def trades(pair, opts \\ []) do
     get(:trades, pair, opts)
+  end
+
+  def ohlc(pair, params \\ [], opts \\ []) do
+    params = params |> Keyword.put_new(:step, 60) |> Keyword.put_new(:limit, 100)
+    pair = process_pair(pair)
+    :ohlc |> url(pair) |> CryptoApis.get(opts ++ [params: params])
+  end
+
+  def pairs(opts \\ []) do
+    :pairs |> url() |> CryptoApis.get(opts)
   end
 end
--- test/bitstamp/bitstamp_test.exs
+++ test/bitstamp/bitstamp_test.exs
@@ -30,8 +30,21 @@ defmodule CryptoApis.BitstampTest do
     end
   end
 
+  describe "hourly_ticker" do
+    test "hourly_ticker/1 responds ok" do
+      with_mock HTTPoison,
+        get: fn url, _headers, options ->
+          {:ok, successful_response(url: url, options: options)}
+        end do
+        assert {:ok, response} = Bitstamp.hourly_ticker("BTCGBP")
+        assert response.status_code == 200
+        assert response.request_url == "https://www.bitstamp.net/api/v2/ticker_hour/btcgbp/"
+      end
+    end
+  end
+
   describe "trades" do
-    test "trades/3 responds ok" do
+    test "trades/1 responds ok" do
       with_mock HTTPoison,
         get: fn url, _headers, options ->
           {:ok, successful_response(url: url, options: options)}
@@ -39,6 +52,63 @@ defmodule CryptoApis.BitstampTest do
         assert {:ok, response} = Bitstamp.trades({:BTC, "GBP"})
         assert response.status_code == 200
         assert response.request_url == "https://www.bitstamp.net/api/v2/transactions/btcgbp/"
+      end
+    end
+  end
+
+  describe "ohlc" do
+    test "ohlc/1 responds ok" do
+      with_mock HTTPoison,
+        get: fn url, _headers, options ->
+          {:ok, successful_response(url: url, options: options)}
+        end do
+        assert {:ok, response} = Bitstamp.ohlc({:BTC, "GBP"})
+        assert response.status_code == 200
+        assert response.request_url == "https://www.bitstamp.net/api/v2/ohlc/btcgbp/"
+        assert response.request.options == [params: [limit: 100, step: 60]]
+      end
+    end
+
+    test "ohlc/2 responds ok" do
+      with_mock HTTPoison,
+        get: fn url, _headers, options ->
+          {:ok, successful_response(url: url, options: options)}
+        end do
+        assert {:ok, response} =
+                 Bitstamp.ohlc({:BTC, "GBP"}, start: "start", end: "end", step: 30, limit: 1)
+
+        assert response.status_code == 200
+        assert response.request_url == "https://www.bitstamp.net/api/v2/ohlc/btcgbp/"
+
+        assert response.request.options == [
+                 params: [start: "start", end: "end", step: 30, limit: 1]
+               ]
+      end
+    end
+  end
+
+  describe "pairs" do
+    test "pairs/0 responds ok" do
+      with_mock HTTPoison,
+        get: fn url, _headers, options ->
+          {:ok, successful_response(url: url, options: options)}
+        end do
+        assert {:ok, response} = Bitstamp.pairs()
+        assert response.status_code == 200
+        assert response.request_url == "https://www.bitstamp.net/api/v2/trading-pairs-info/"
+      end
+    end
+  end
+
+  describe "eur_usd_rate" do
+    test "eur_usd_rate/0 responds ok" do
+      with_mock HTTPoison,
+        get: fn url, _headers, options ->
+          {:ok, successful_response(url: url, options: options)}
+        end do
+        assert {:ok, response} = Bitstamp.eur_usd_rate()
+        assert response.status_code == 200
+        assert response.request_url == "https://www.bitstamp.net/api/v2/eur_usd/"
       end
     end
   end
