6672488d6881d4628b1bd62015c5a45e341b54f3
Hugo Morgan <hpjm37@gmail.com>
2021-03-08 16:37:10 +0000

    refactor indodax

--- lib/indodax/indodax.ex
+++ lib/indodax/indodax.ex
@@ -5,14 +5,14 @@ defmodule CryptoApis.Indodax do
 
   @root_url "https://indodax.com/api"
 
-  alias CryptoApis
+  alias CryptoApis.Pair
 
-  defp get_pair({_, _} = pair), do: pair
-  defp get_pair(pair), do: CryptoApis.Utils.split_pair(pair)
+  defp process_pair(pair) do
+    pair |> Pair.new() |> Pair.join(downcase?: true)
+  end
 
   defp get(type, pair, opts) do
-    {crypto, fiat} = get_pair(pair)
-    pair = String.downcase("#{crypto}#{fiat}")
+    pair = process_pair(pair)
 
     type
     |> url(pair)
@@ -23,10 +23,87 @@ defmodule CryptoApis.Indodax do
     "#{@root_url}/depth/#{pair}"
   end
 
+  defp url(:trades, pair) do
+    "#{@root_url}/trades/#{pair}"
+  end
+
+  defp url(:ticker, pair) do
+    "#{@root_url}/ticker/#{pair}"
+  end
+
+  defp url(:ticker_all) do
+    "#{@root_url}/ticker_all"
+  end
+
+  defp url(:server_time) do
+    "#{@root_url}/server_time/"
+  end
+
+  defp url(:pairs) do
+    "#{@root_url}/pairs/"
+  end
+
+  defp url(:price_increments) do
+    "#{@root_url}/price_increments/"
+  end
+
+  defp url(:summaries) do
+    "#{@root_url}/summaries/"
+  end
+
   @doc """
   https://github.com/btcid/indodax-official-api-docs/blob/master/Public-RestAPI.md#depth
   """
   def order_book(pair, opts \\ []) do
     get(:orders, pair, opts)
+  end
+
+  @doc """
+  https://github.com/btcid/indodax-official-api-docs/blob/master/Public-RestAPI.md#trades
+  """
+  def trades(pair, opts \\ []) do
+    get(:trades, pair, opts)
+  end
+
+  @doc """
+  https://github.com/btcid/indodax-official-api-docs/blob/master/Public-RestAPI.md#ticker
+  """
+  def ticker(pair, opts \\ []) do
+    get(:ticker, pair, opts)
+  end
+
+  @doc """
+  https://github.com/btcid/indodax-official-api-docs/blob/master/Public-RestAPI.md#server-time
+  """
+  def server_time(opts \\ []) do
+    :server_time |> url() |> CryptoApis.get(opts)
+  end
+
+  @doc """
+  https://github.com/btcid/indodax-official-api-docs/blob/master/Public-RestAPI.md#price-increments
+  """
+  def price_increments(opts \\ []) do
+    :price_increments |> url() |> CryptoApis.get(opts)
+  end
+
+  @doc """
+  https://github.com/btcid/indodax-official-api-docs/blob/master/Public-RestAPI.md#pairs
+  """
+  def pairs(opts \\ []) do
+    :pairs |> url() |> CryptoApis.get(opts)
+  end
+
+  @doc """
+  https://github.com/btcid/indodax-official-api-docs/blob/master/Public-RestAPI.md#summaries
+  """
+  def summaries(opts \\ []) do
+    :summaries |> url() |> CryptoApis.get(opts)
+  end
+
+  @doc """
+  https://github.com/btcid/indodax-official-api-docs/blob/master/Public-RestAPI.md#ticker-all
+  """
+  def ticker_all(opts \\ []) do
+    :ticker_all |> url() |> CryptoApis.get(opts)
   end
 end
--- test/indodax/indodax_test.exs
+++ test/indodax/indodax_test.exs
@@ -18,4 +18,109 @@ defmodule CryptoApis.IndodaxTest do
       end
     end
   end
+
+  describe "trades" do
+    test "trades/1 responds ok" do
+      with_mock HTTPoison,
+        get: fn url, _headers, options ->
+          {:ok, successful_response(url: url, options: options)}
+        end do
+        assert {:ok, response} = Indodax.trades(:BTCIDR)
+        assert response.status_code == 200
+
+        assert response.request_url ==
+                 "https://indodax.com/api/trades/btcidr"
+      end
+    end
+  end
+
+  describe "ticker" do
+    test "ticker/1 responds ok" do
+      with_mock HTTPoison,
+        get: fn url, _headers, options ->
+          {:ok, successful_response(url: url, options: options)}
+        end do
+        assert {:ok, response} = Indodax.ticker(:BTCIDR)
+        assert response.status_code == 200
+
+        assert response.request_url ==
+                 "https://indodax.com/api/ticker/btcidr"
+      end
+    end
+  end
+
+  describe "ticker_all" do
+    test "ticker_all/0 responds ok" do
+      with_mock HTTPoison,
+        get: fn url, _headers, options ->
+          {:ok, successful_response(url: url, options: options)}
+        end do
+        assert {:ok, response} = Indodax.ticker_all()
+        assert response.status_code == 200
+
+        assert response.request_url ==
+                 "https://indodax.com/api/ticker_all"
+      end
+    end
+  end
+
+  describe "server_time" do
+    test "server_time/0 responds ok" do
+      with_mock HTTPoison,
+        get: fn url, _headers, options ->
+          {:ok, successful_response(url: url, options: options)}
+        end do
+        assert {:ok, response} = Indodax.server_time()
+        assert response.status_code == 200
+
+        assert response.request_url ==
+                 "https://indodax.com/api/server_time/"
+      end
+    end
+  end
+
+  describe "pairs" do
+    test "pairs/0 responds ok" do
+      with_mock HTTPoison,
+        get: fn url, _headers, options ->
+          {:ok, successful_response(url: url, options: options)}
+        end do
+        assert {:ok, response} = Indodax.pairs()
+        assert response.status_code == 200
+
+        assert response.request_url ==
+                 "https://indodax.com/api/pairs/"
+      end
+    end
+  end
+
+  describe "price_increments" do
+    test "price_increments/0 responds ok" do
+      with_mock HTTPoison,
+        get: fn url, _headers, options ->
+          {:ok, successful_response(url: url, options: options)}
+        end do
+        assert {:ok, response} = Indodax.price_increments()
+        assert response.status_code == 200
+
+        assert response.request_url ==
+                 "https://indodax.com/api/price_increments/"
+      end
+    end
+  end
+
+  describe "summaries" do
+    test "summaries/0 responds ok" do
+      with_mock HTTPoison,
+        get: fn url, _headers, options ->
+          {:ok, successful_response(url: url, options: options)}
+        end do
+        assert {:ok, response} = Indodax.summaries()
+        assert response.status_code == 200
+
+        assert response.request_url ==
+                 "https://indodax.com/api/summaries/"
+      end
+    end
+  end
 end
